0df214522262c5a1c3ba0512c48895719c940859
chisoftltd <chisoftltd@gmail.com>
2020-05-29 20:09:21 +0200

    Add project files.

---
+++ MongoDBChisoftApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDBChisoftApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+
+            PersonModel person = new PersonModel
+            {
+                FirstName = "Mikael",
+                LastName = "Chinwe",
+                PrimaryAddress = new AddressModel
+                {
+                    StreetAddress = " 8 Church Street",
+                    City = "Baillieston",
+                    State = "Glasgow",
+                    PostCode = "G69 7NU"
+                }
+            };
+
+            db.InsertRecord("Users", person);
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+    }
+
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+
+    }
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+    }
+}
