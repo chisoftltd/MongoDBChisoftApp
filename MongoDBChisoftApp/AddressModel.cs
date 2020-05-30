50eb44938440dc8b6bf511619ff7f20ff36e2019
chisoftltd <chisoftltd@gmail.com>
2020-05-30 02:31:13 +0200

    MongoDB Object Programming

---
+++ MongoDBChisoftApp/AddressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MongoDBChisoftApp
+{
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+
+    }
+}
--- MongoDBChisoftApp/Program.cs
+++ MongoDBChisoftApp/Program.cs
@@ -1,8 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace MongoDBChisoftApp
 {
@@ -57,80 +53,6 @@ namespace MongoDBChisoftApp
             //db.deleteRecord<PersonModel>("Users", oneRec.Id);
 
             Console.ReadLine();
-        }
-    }
-
-    public class NameModel
-    {
-        [BsonId]
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-
-    public class PersonModel
-    {
-        [BsonId]
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public AddressModel PrimaryAddress { get; set; }
-        [BsonElement("dob")]
-        public DateTime DateOfBirth { get; set; }
-    }
-
-    public class AddressModel
-    {
-        public string StreetAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostCode { get; set; }
-
-    }
-    public class MongoCRUD
-    {
-        private IMongoDatabase db;
-
-        public MongoCRUD(string database)
-        {
-            var client = new MongoClient();
-            db = client.GetDatabase(database);
-        }
-
-        public void InsertRecord<T>(string table, T record)
-        {
-            var collection = db.GetCollection<T>(table);
-            collection.InsertOne(record);
-        }
-
-        public List<T> LoadRecord<T>(string table)
-        {
-            var collection = db.GetCollection<T>(table);
-            return collection.Find(new BsonDocument()).ToList();
-        }
-
-        public T LoadRecordById<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-
-            return collection.Find(filter).First();
-        }
-
-        public void UpsertRecord<T>(string table, Guid id, T record)
-        {
-            var collection = db.GetCollection<T>(table);
-            var result = collection.ReplaceOne(
-                new BsonDocument("_id", id),
-                record,
-                new UpdateOptions { IsUpsert = true });
-        }
-
-        public void deleteRecord<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-            collection.DeleteOne(filter);
         }
     }
 }
